43212b608db65a6b3dcdfcd1aa1021b3dccfb3aa
Yusef <Yusef@Yusefs-Laptop.local>
2023-10-03 22:06:11 +0100

    adding same site for cookie

--- src/controllers/auth-controllers.ts
+++ src/controllers/auth-controllers.ts
@@ -24,9 +24,10 @@ export const loginUser = async (
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      // secure: true,
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+    // secure: true,
 
     res.status(200).send({ accessToken });
   } catch (err) {
